f5718191737765553690e6796d7bb253b2c18fd7
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-14 16:22:13 -0500

    initial setup

---
+++ src/js/mercury.js
@@ -0,0 +1,16 @@
+'use strict';
+let Mercury = {};
+
+
+
+
+// Name
+// Year discovered
+// Mass
+// Size
+// Distance from Sun
+// Atmosphere composition
+// Satellites
+// Name of any probes/orbiters/explorers that have visited or landed on that planet
+
+module.exports = Mercury;
